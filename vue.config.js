e74995b7f9d05719474c432556e8e9c63476eacd
orange <zsw3166483606@163.com>
2024-02-02 14:18:05 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  // 将依赖包提前编译
+  transpileDependencies: true,
+    // 配置webpack
+  chainWebpack: config => {
+    config
+        .plugin('html')
+        .tap(args => {
+          // 修改title
+          args[0].title = 'Orange';
+          return args;
+        })
+  }
 })
+
