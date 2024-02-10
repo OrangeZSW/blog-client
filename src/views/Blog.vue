9c70b09d22698f2a4b3ccc68daa45619e68f18bf
orange <zsw3166483606@163.com>
2024-02-10 22:17:29 +0800

    version-2

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -14,7 +14,7 @@
 <script>
 import Header from "@/components/Header.vue";
 import Center from "@/components/Center.vue";
-import Login from "@/components/login.vue";
+import Login from "@/components/Login.vue";
 export default {
   components: {
     Login,
