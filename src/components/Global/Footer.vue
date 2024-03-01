0a1542a8323535e8b003e3ea9b311c6c48299502
orange <zsw3166483606@163.com>
2024-03-01 19:35:27 +0800

    完成footer

--- src/components/Global/Footer.vue
+++ src/components/Global/Footer.vue
@@ -6,12 +6,34 @@ export default {
 
 <template>
   <div class="footer"  >
-    <h1>wdadwa</h1>
+    <div class="footer-info">
+      <span>© 2023-2024 OrangeZSW</span>
+      <span>Powered by <a id="a"  href="https://v2.cn.vuejs.org/v2/guide/" target="_blank">vue</a></span>
+      <span>welcome to my <img  src="https://cdn.jsdelivr.net/gh/OrangeZSW/blog_img/背景/爱心.png" alt=""> blog</span>
+      <a href="https://beian.miit.gov.cn/" id="a" target="_blank">粤ICP备202305021008781号</a>
+    </div>
   </div>
 </template>
 
 <style scoped>
+#a:hover{
+  text-decoration: underline;
+  color: white;
+}
+#a{
+  color: white;
+  text-decoration: none;
+}
+.footer-info{
 
+  color: white;
+  z-index: 1;
+  margin: 10px 40% 10px 40%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 .footer{
   width: 100%;
   height: 194px;
@@ -23,5 +45,15 @@ export default {
   background-image: url("https://cdn.jsdelivr.net/gh/OrangeZSW/blog_img/202305021008781.png");
   background-size: cover; /* 可根据需要调整 */
   background-position: center bottom; /* 将背景图片底部对齐 */
+}
+.footer:before {
+  content: '';
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 194px;
+  z-index: 0;
+  background-color: rgba(0, 0, 0, 0.5);
 }
 </style>
